6ab2a4f5401bdaea2c29e9d04ab4f6232426eca4
EorEquis <eeyore@stuffupthere.com>
2018-01-26 20:51:34 -0600

    Initial Commit

---
+++ TriStar_Dew_Heater.ino
@@ -0,0 +1,54 @@
+/******************************
+* Simple dew heater controller.
+* 
+* Reads a DHT22 for humidity, and if humidity is
+* over a certain level, turns the dew heater on.
+* Otherwise, turns it off.
+*******************************/
+
+//Libraries
+#include <DHT.h>;
+
+//Constants
+#define DHTPIN 7              // DHT Data pin
+#define DHTTYPE DHT22         // DHT 22  (AM2302)
+#define heaterPin 9           // Base pin on TIP120
+DHT dht(DHTPIN, DHTTYPE);     // Initialize DHT
+
+
+//Variables
+float hum;                    // Stores humidity value
+float humLimit = 28;          // Max humidity before heater is enabled.  Float for easier testing, could be int
+int readDelay = 2000;         // Milliseconds between reads.  The heater could, conceivably, cycle every time DHT is read.  Set with caution.
+void setup()
+{
+  Serial.begin(9600);         // Needed for initial testing/troubleshooting
+  dht.begin();
+}
+
+void loop()
+{
+    // Read humidity and store in hum
+    hum = dht.readHumidity();
+
+    //Print humidity and heater state for testing
+    if (hum > humLimit)
+      {
+        analogWrite(heaterPin, 255);
+        Serial.print("Humidity: ");
+        Serial.print(hum);
+        Serial.println(" % - Heater is on.");
+      }
+    else
+      {
+        analogWrite(heaterPin, 0);
+        Serial.print("Humidity: ");
+        Serial.print(hum);
+        Serial.println(" % - Heater is off.");
+      }
+
+    // Delay before reading again
+    delay(readDelay); 
+}
+
+    
